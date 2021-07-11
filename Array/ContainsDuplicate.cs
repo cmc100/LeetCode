b005bf337313dfa6a46a0b01da7c6dae2eee890f
Mike Cole <mike@planetcole.com>
2021-07-11 14:36:42 -0400

    Add ContainsDuplicate

---
+++ Array/ContainsDuplicate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+namespace LeetCode.Array
+{
+    public class ContainsDuplicateTests
+    {
+        [Theory]
+        [InlineData(new[] { 7, 1, 5, 3, 6, 4 }, false)]
+        [InlineData(new[] { 7, 3, 4, 3, 1 }, true)]
+        public void ShouldPass(int[] nums, bool dupe)
+        {
+            var answer = ContainsDuplicateAnswer.ContainsDuplicate(nums);
+            Assert.Equal(answer, dupe);
+        }
+    }
+
+    public class ContainsDuplicateAnswer
+    {
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            var set = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (set.Contains(nums[i]))
+                    return true;
+
+                set.Add(nums[i]);
+            }
+            return false;
+        }
+    }
+}
